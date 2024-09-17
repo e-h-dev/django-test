271f0986d1769302afeb3f887e93c8d4e3fbb407
Eli Hammond <ebaal8226@gmail.com>
2024-09-17 11:31:37 +0000

    Create test coverage

--- todo/test_views.py
+++ todo/test_views.py
@@ -44,3 +44,22 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
+
+
+    # def test_can_edit_item(self):
+    #     item = Item.objects.create(name='Test Todo Item')
+    #     response = self.client.post(f'/toggle/{item.id}', {'name': 'Updated Name'})
+    #     self.assertRedirects(response, '/')
+    #     updated_item = Item.objects.get(id=item.id)
+    #     self.assertEqual(updated_item.name, 'Updated name')
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
+
+
+
+
