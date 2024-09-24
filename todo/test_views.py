2821ffe686238a7d94548758ea1df2e7b4ca58d7
Eli Hammond <ebaal8226@gmail.com>
2024-09-24 08:45:23 +0000

    Create can edit item test

--- todo/test_views.py
+++ todo/test_views.py
@@ -46,13 +46,6 @@ class TestViews(TestCase):
         self.assertFalse(updated_item.done)
 
 
-    # def test_can_edit_item(self):
-    #     item = Item.objects.create(name='Test Todo Item')
-    #     response = self.client.post(f'/toggle/{item.id}', {'name': 'Updated Name'})
-    #     self.assertRedirects(response, '/')
-    #     updated_item = Item.objects.get(id=item.id)
-    #     self.assertEqual(updated_item.name, 'Updated name')
-
     def test_can_edit_item(self):
         item = Item.objects.create(name='Test Todo Item')
         response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
